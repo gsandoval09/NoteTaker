ac5ed10c82dcc460e669ff918fe584da730aeaf7
George Sandoval <gssandoval09@gmail.com>
2021-10-21 22:26:56 -0500

    made delete button work

--- homework/Develop/Routes/apiroute.js
+++ homework/Develop/Routes/apiroute.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const action = require("../db/action")
-router.get("/notes",(req, res) => {
-    action.getNotes().then(notes => res.json(notes))
-  })
-  router.post("/notes",(req, res) => {
+router.get("/notes", (req, res) => {
+  action.getNotes().then(notes => res.json(notes))
+})
+// router.get("/notes/:id", (req, res) => {
+//   action.getNotes().then(notes =>{
+//   // let noteID = 
+//   res.json(notes[req.params.id])})
+// })
+
+
+
+router.post("/notes", (req, res) => {
   action.addNote(req.body)
-  .then(notes => res.json(notes))
-  });
+    .then(notes => res.json(notes))
+});
+//action to delete note 
+router.delete("/notes/:id", (req, res) => {
+  action.deleteNote(req.params.id).then(notes => res.json(notes))
+  console.info("recieved delete request")
+});
 
 
-  module.exports = router
+
+module.exports = router
 
 // const app = require('express').Router() 
 // const path = require('path')
